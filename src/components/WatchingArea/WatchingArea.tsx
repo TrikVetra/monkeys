1a4b59385d29d5377088f9708dcc427f4f0b2044
Tanya <tastr@outlook.com>
2023-01-23 16:30:26 +0300

    pointer fixed

--- src/components/WatchingArea/WatchingArea.tsx
+++ src/components/WatchingArea/WatchingArea.tsx
@@ -6,30 +6,28 @@ const WatchingArea = () => {
     let counter: number = -1
     const identicText = useAppSelector(state => state.Text.identicText)
     const chars = [...identicText]
-    const data = chars.map(el => {   
-        counter++     
-        return {place: counter, element: el}
+    const data = chars.map(el => {
+        counter++
+        return { place: counter, element: el }
     })
 
     const pointer = useAppSelector(state => state.Text.pointer)
 
     return (
         <>
-            {pointer}
             <h5>Если угадал с буквами, увидишь их тут</h5>
             <div className={styles.area}>
-                {data.map(el => {                    
-                    if (el.place === pointer) {return <div key={el.place} className={styles.pointer}>{el.element}</div>}
-                    else {return <div key={el.place}>{el.element}</div>}
+                {data.map(el => {
+                    return (
+                        <div
+                            key={el.place}
+                            className={el.place === pointer ? styles.pointer : ""}
+                        >
+                            {el.element}
+                        </div>
+                    )
                 })}
             </div>
-            {/* <Form.Control 
-                style={{height: "50vh", backgroundColor: "#D3D3D3"}}
-                as="textarea" 
-                aria-label="With textarea" 
-                value={String(identicText)} 
-                readOnly
-            /> */}
         </>
     )
 }
